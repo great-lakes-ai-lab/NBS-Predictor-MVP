84eea7ccfa6a774b1154736bd3a31749599ee15b
Matt McAnear <7784900+mcanearm@users.noreply.github.com>
2024-05-31 12:14:25 -0400

    test select against local database

--- tests/test_db_utils.py
+++ tests/test_db_utils.py
@@ -1,5 +1,16 @@
+import pytest
+
 from src.step1_data_loading.db_utils import get_dsn
-from sqlalchemy import create_engine, text
+from src.step1_data_loading.data_loading import Runoff
+from sqlalchemy import create_engine, text, select
+
+import pandas as pd
+
+
+@pytest.fixture
+def engine(environ):
+    engine = create_engine(get_dsn())
+    return engine
 
 
 def test_get_dsn(environ):
@@ -8,9 +19,18 @@ def test_get_dsn(environ):
     assert dsn
 
 
-def test_local_select(environ):
+def test_local_select(engine):
 
-    engine = create_engine(get_dsn())
     with engine.connect() as conn:
         res = conn.execute(text("select 10")).fetchone()
     assert res[0] == 10
+
+
+def test_simple_select(engine):
+    # Example usage of a SQLAlchemy select
+    with engine.connect() as conn:
+        stmt = select(Runoff).limit(10)
+        rows = pd.read_sql(stmt, con=conn, index_col="date")
+
+    assert isinstance(rows, pd.DataFrame)
+    assert rows.shape == (10, 4)
