4f1d50605d0a95ecc2d389e8060399625dd1975a
Matt McAnear <7784900+mcanearm@users.noreply.github.com>
2024-06-12 10:36:32 -0400

    reduce src namespace

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 # Import functions/classes from individual steps
-from .step1_data_loading import *
-from .step2_preprocessing.preprocessing import *
-from .step3_modeling.modeling import *
-from .step4_postprocessing.postprocessing import *
+import step1_data_loading
+import step2_preprocessing
+import step3_modeling
+import step4_postprocessing
