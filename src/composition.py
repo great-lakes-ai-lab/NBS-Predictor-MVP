1bfb2662f8a6de525a7ae30a38da8ffaa1bc323b
DaniJonesOcean <dcjones.work@gmail.com>
2024-09-17 14:16:30 -0400

    Restored src/data_loading/data_loading.py and src/composition.py from commit d7273daae6b8070f19f2b763fd0ec83260e88658

---
+++ src/composition.py
@@ -0,0 +1,52 @@
+from typing import Union
+
+from sklearn.pipeline import Pipeline
+
+from modeling.modeling import ModelBase
+from postprocessing.postprocessing import PostprocessingPipeline
+
+__all__ = ["ModelPipeline"]
+
+
+class ModelPipeline(object):
+    """
+    A pipeline object that composes a preprocessor, model, and postprocessor together
+    to make predictions from the lake data using a single object. This differs from
+    the scikit-learning pipeline by applying the postprocessing steps to model predictions.
+    However, scikit-learn pipelines are supported for both the model object and preprocessor.
+
+    Attributes:
+    -----------
+
+    preprocessor : Pipeline
+        The preprocessing pipeline.
+
+    model : ModelBase
+        The base machine learning model. Shoul
+
+    postprocessor: PostprocessingPipeline, optional
+        The postprocessing pipeline. Defaults to None.
+
+    """
+
+    def __init__(
+        self,
+        model: Union[ModelBase, Pipeline],
+        preprocessor: Pipeline = None,
+        postprocessor: PostprocessingPipeline = None,
+    ):
+        self.preprocessor = preprocessor
+        self.model = model
+        self.postprocessor = postprocessor
+        self.predictive_model = Pipeline(
+            [("preprocess", self.preprocessor), ("model", self.model)]
+        )
+
+    def fit(self, X, y, *args, **kwargs):
+        return self.predictive_model.fit(X, y, *args, **kwargs)
+
+    def predict(self, X, y=None, *args, **kwargs):
+        return self.predictive_model.predict(X, y=y, *args, **kwargs)
+
+    def fit_predict(self, X, y, *args, **kwargs):
+        return self.predictive_model.fit_predict(X, y, *args, **kwargs)
