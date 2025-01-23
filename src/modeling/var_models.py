405fba9f70d1a1b958a1d93f927648bbd04b2552
Matt McAnear <7784900+mcanearm@users.noreply.github.com>
2025-01-23 14:23:15 -0500

    in progress bugfixes for VARX and VAR

--- src/modeling/var_models.py
+++ src/modeling/var_models.py
@@ -1,9 +1,9 @@
 import calendar
-from functools import reduce
+import logging
+from abc import abstractmethod
 from typing import Union
 
 import jax
-import numpy as np
 import numpyro
 from jax import numpy as jnp
 from numpyro import distributions as dist
@@ -11,20 +11,34 @@ from numpyro.contrib.control_flow import scan
 from numpyro.infer.reparam import LocScaleReparam
 
 from src.modeling.modeling import NumpyroModel
-from src.utils import flatten_array, lag_array
+
+# Configure logging
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.DEBUG)
 
 __all__ = [
     # Classes
     "VAR",
+    "VARX",
     "NARX",
 ]
 
 
-class VAR(NumpyroModel):
+class LinearAR(NumpyroModel):
+    """Helper class for VARX and VAR models, since everything is shared except for the use of
+    covariates.
+
+    Args:
+        NumpyroModel (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
 
     @property
+    @abstractmethod
     def name(self):
-        return "VAR"
+        pass
 
     def __init__(
         self,
@@ -36,7 +50,7 @@ class VAR(NumpyroModel):
     ):
         super().__init__()
         if lags is None:
-            self.lags = {"y": 3, "precip": 0}
+            self.lags = {"y": 3}
         else:
             self.lags = lags
         self.num_chains = num_chains
@@ -46,6 +60,29 @@ class VAR(NumpyroModel):
         self.predictive_fn = None  # updated during model fitting
         self.trace = None
         self.mcmc = None
+
+    @property
+    @abstractmethod
+    def coords(self):
+        pass
+
+    @property
+    @abstractmethod
+    def dims(self):
+        pass
+
+    def load(cls, path):
+        pass
+
+    def save(self, path):
+        pass
+
+
+class VAR(LinearAR):
+
+    @property
+    def name(self):
+        return "VAR"
 
     @property
     def coords(self):
@@ -63,28 +100,18 @@ class VAR(NumpyroModel):
             "sigma": ["lakes"],
             "corr": ["series", "lakes"],
             "theta": ["lakes"],
-            **{
-                f"{k}_alpha": (
-                    ["series", "lakes", "lag"] if k == "y" else ["series", "lakes"]
-                )
-                for k in self.lags.keys()
-            },
         }
-
-    def load(cls, path):
-        pass
-
-    def save(self, path):
-        pass
 
     @staticmethod
     @numpyro.handlers.reparam(config={"intercept": LocScaleReparam(0)})
-    def model(y, y_index, lags, covariates, future=0):
+    def model(y, y_index, lags, future=0, **kwargs):
         """
         Autoregressive process.
         Args:
             y: the time series to fit
-            covariates: An XArray of covariates for use in the model. Leading index should be date, second index should be lake, and the third index is the actual covariate values.
+            covariates: An XArray of covariates for use in the model. Leading
+                index should be date, second index should be lake, and the third
+                index is the actual covariate values.
             lags: A dictionary indicating which covariates have which lags
             future: How many periods to run into the future.
 
@@ -96,16 +123,14 @@ class VAR(NumpyroModel):
         nu = numpyro.sample("nu", dist.HalfNormal(10.0))
 
         ar_lag = max_lag = lags.get("y")
-        covars = jnp.array(covariates)
 
-        covar_alphas = numpyro.sample(
-                f"alpha_lake",
-                dist.Normal(0, 0.5),
-                sample_shape=(4, (covars.shape[-1], 4))
-        )
         theta = numpyro.sample("theta", dist.HalfNormal(5), sample_shape=(4,))
 
         with numpyro.plate("lakes", size=4):
+            with numpyro.plate("other_lake", size=4):
+                with numpyro.plate("ar_terms", size=ar_lag):
+                    ar_alpha = numpyro.sample("ar_alpha", dist.Normal(0, 0.5))
+
             with numpyro.plate("months", size=12):
                 intercept = numpyro.sample("intercept", dist.Normal(global_mu, 1))
 
@@ -116,22 +141,17 @@ class VAR(NumpyroModel):
 
         def transition_fn(carry, covars):
             prev_y = carry
-            month_t = covars[0]
+            month_t = covars
 
-            lagged_series = [prev_y, covars]
+            # final answer
+            m_t = jnp.zeros((4,))
 
-            m = jnp.zeros((4,))
+            # get the effect of the lagged values
+            y_lag = lags.get("y")
+            for t in range(y_lag):
+                m_t += jnp.matmul(ar_alpha[t], carry[t, :])
 
-            for i in range(len(lags.items())):
-                alphas = covar_alphas[i]
-                dataset = lagged_series[i]
-                if len(alphas.shape) > 2:
-                    for j in jnp.arange(alphas.shape[-1]):
-                        m += jnp.matmul(alphas[:, :, j], dataset[j, :])
-                else:
-                    m += jnp.matmul(alphas, dataset)
-
-            m_t = intercept[month_t, :] + m
+            m_t += intercept[month_t, :]
             y_t = numpyro.sample(
                 "y", dist.MultivariateStudentT(df=nu, loc=m_t, scale_tril=L_Omega)
             )
@@ -147,7 +167,139 @@ class VAR(NumpyroModel):
         months = jnp.array(y_index.month - 1)
         initial_values = prev
 
-        covars = (months[max_lag:], covars)
+        if future > 0:
+            y_fit = y[:-future]
+        else:
+            y_fit = y
+
+        with numpyro.handlers.condition(data={"y": y_fit[max_lag:]}):
+            _, ys = scan(transition_fn, initial_values, months[max_lag:])
+
+        if future > 0:
+            numpyro.deterministic("y_forecast", ys[-future:])
+
+
+class VARX(LinearAR):
+
+    def __init__(
+        self,
+        lags=None,
+        num_chains=4,
+        num_samples=1000,
+        num_warmup=1000,
+        lakes=("sup", "mic_hur", "eri", "ont"),
+    ):
+        super().__init__(lags, num_chains, num_samples, num_warmup, lakes)
+        self.covars = None
+
+    @property
+    def name(self):
+        return "VARX"
+
+    @property
+    def coords(self):
+        return {
+            "series": self.lakes,
+            "lakes": self.lakes,
+            "month": list(calendar.month_abbr)[1:],
+            "lag": range(1, self.lags["y"] + 1),
+            # TODO: this is terrible practice, but hard code to dimensions of three variables
+            # It's necessary because the "model" method MUST be a static method to work with JAX and compilation.
+            "covars": list(range(3)),
+        }
+
+    @property
+    def dims(self):
+        return {
+            "intercept": ["month", "lakes"],
+            "sigma": ["lakes"],
+            "corr": ["series", "lakes"],
+            "theta": ["lakes"],
+            "beta_covar": ["covars", "lakes", "series"],
+            "ar_alpha": ["lag", "lakes", "series"],
+        }
+
+    def load(cls, path):
+        pass
+
+    def save(self, path):
+        pass
+
+    @staticmethod
+    @numpyro.handlers.reparam(config={"intercept": LocScaleReparam(0)})
+    def model(y, y_index, lags, covariates, future=0):
+        """
+        Autoregressive process.
+        Args:
+            y: the time series to fit
+            covariates: An XArray of covariates for use in the model. Leading
+                index should be date, second index should be lake, and the third
+                index is the actual covariate values.
+            lags: A dictionary indicating which covariates have which lags
+            future: How many periods to run into the future.
+
+        Returns:
+            None - samples
+
+        """
+        global_mu = numpyro.sample("global_mu", dist.Normal(0, 1))
+        nu = numpyro.sample("nu", dist.HalfNormal(10.0))
+
+        ar_lag = max_lag = lags.get("y")
+        covars = jnp.array(covariates)
+
+        theta = numpyro.sample("theta", dist.HalfNormal(5), sample_shape=(4,))
+
+        with numpyro.plate("lakes", size=4):
+            with numpyro.plate("other_lake", size=4):
+                with numpyro.plate("series", covars.shape[-1]):
+                    beta_lake = numpyro.sample("beta_var", dist.Normal(0, 0.5))
+                with numpyro.plate("ar_terms", size=ar_lag):
+                    ar_alpha = numpyro.sample("ar_alpha", dist.Normal(0, 0.5))
+
+            with numpyro.plate("months", size=12):
+                intercept = numpyro.sample("intercept", dist.Normal(global_mu, 1))
+
+        # t_nu = numpyro.sample("t_nu", dist.HalfNormal(10))
+        l_omega = numpyro.sample("corr", dist.LKJCholesky(4, concentration=0.5))
+        sigma = jnp.sqrt(theta)
+        L_Omega = sigma[..., None] * l_omega
+
+        def transition_fn(carry, covars):
+            prev_y = carry
+            month_t, covariate_mat = covars
+
+            # final answer
+            m_t = jnp.zeros((4,))
+
+            # get the effect of the lagged values
+            y_lag = lags.get("y")
+            for t in range(y_lag):
+                m_t += jnp.matmul(ar_alpha[t], carry[t, :])
+
+            # get the effect of the covariates - note that this does not allow
+            # lags of the covariates directly, though this can be implemented
+            # simply adding those as features
+            for j in range(beta_lake.shape[0]):
+                m_t += jnp.matmul(beta_lake[j, :, :], covariate_mat[:, j])
+
+            m_t += intercept[month_t, :]
+            y_t = numpyro.sample(
+                "y", dist.MultivariateStudentT(df=nu, loc=m_t, scale_tril=L_Omega)
+            )
+
+            if ar_lag > 1:
+                new_vals = jnp.append(prev_y[1:], y_t.reshape(1, -1), axis=0)
+            else:
+                new_vals = y_t.reshape(1, -1)
+            return new_vals, y_t
+
+        prev = y[:max_lag][-ar_lag:]
+        # need to subtract one because indexing starts at 0.
+        months = jnp.array(y_index.month - 1)
+        initial_values = prev
+
+        covars = (months[max_lag:], covars[max_lag:])
 
         if future > 0:
             y_fit = y[:-future]
@@ -165,7 +317,7 @@ class NARX(NumpyroModel):
 
     def __init__(self, lags=None, num_chains=4, num_samples=1000, num_warmup=1000):
         super().__init__(lags, num_chains, num_samples, num_warmup)
-        self.lags = lags or {"y": 3, "evap": 2, "precip": 2}
+        self.lags = lags or {"y": 3}
 
     @property
     def name(self):
@@ -193,6 +345,8 @@ class NARX(NumpyroModel):
             None - samples
 
         """
+        logger.debug("Entering NARX model method")
+
         nu = numpyro.sample("nu", dist.HalfNormal(10.0))
 
         ar_lag = max_lag = lags.get("y")
@@ -262,3 +416,5 @@ class NARX(NumpyroModel):
 
         if future > 0:
             numpyro.deterministic("y_forecast", ys[-future:])
+
+        logger.debug("Exiting NARX model method")
